5a228c69d9bee2f6b44c5846207b2344ed759a2e
klapvm <84770632+klapvm@users.noreply.github.com>
2021-05-27 12:31:54 +0500

    Add files via upload

---
+++ Pract9BurKla/Pract9BurKla/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Pract9BurKla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            {
+                int[,] m = new int[8, 8];
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        m[i, j] = 1 - (i + j) % 2;
+                        Console.Write(m[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            //2
+            {
+                int[,] m = new int[10, 10];
+                Random rnd = new Random();
+                int sum = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        m[i, j] = rnd.Next(1, 99);
+                        Console.Write("\t" + m[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        sum += m[i, j];
+                    }
+                    Console.WriteLine($"Сумма {i+1} строки = " + sum);
+                    sum = 0;
+                }
+            }
+        }
+    }
+}
